6b82497daf4618426509b30b62117eccea282bbe
Andrew Bonner <andrewbonner92@gmail.com>
2017-11-11 10:11:19 -0500

    Set up and test mongoose.

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost:27017/TodoApp')
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+})
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// })
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc)
+// }, (err) => {
+//   console.log('Unable to save todo')
+// })
+
+var anotherTodo = new Todo({
+  text: 'Make new todo',
+  completed: true,
+  completedAt: 123
+}).save().then((doc) => {
+  console.log('Saved todo', doc)
+}, (err) => {
+  console.log('Unable to save todo')
+})
