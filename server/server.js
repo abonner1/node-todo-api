1053fceb3f7c24bf27aa9591625f02918cba4dde
Andrew Bonner <andrewbonner92@gmail.com>
2017-11-13 14:31:06 -0500

    Setup basic express server.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
+var express = require('express')
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+
+var app - express()
+
+app.listen(3000, () => {
+  console.log('Started on port 3000')
+})
