74ac0ed8da0114e1fb52955bb0b4d683d7d4db22
Andrew Bonner <andrewbonner92@gmail.com>
2017-11-13 14:48:50 -0500

    Add post todos route.

--- server/server.js
+++ server/server.js
@@ -5,7 +5,26 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
 
-var app - express()
+var app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  })
+
+  todo.save()
+    .then((doc) => {
+      res.send(doc)
+    }, (err) => {
+      res.status(400).send(err)
+    })
+})
+
+// app.get('/todos', (req, res) => {
+//
+// })
 
 app.listen(3000, () => {
   console.log('Started on port 3000')
